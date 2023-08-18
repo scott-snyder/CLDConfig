8e10c81834cce634b4574ac3ed93041f54d30826
Leonhard Reichenbach <leonhard.reichenbach@cern.ch>
2023-08-18 16:54:54 +0200

    remove test output limit

--- test/test_CLDConfig.py
+++ test/test_CLDConfig.py
@@ -12,4 +12,4 @@ def _CLDConfig(test_fun):
 @_CLDConfig
 def test_lcio_edm4hep():
     res = subprocess.run("k4run --inputFile=../test.slcio --outputBasename=rec_test CLDReconstruction.py".split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-    assert res.returncode == 0, res.stdout[-(30*80):]
+    assert res.returncode == 0, res.stdout
