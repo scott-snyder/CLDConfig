bbc0b1515270ef78d1aad8878c14378ad532f1ef
Andre Sailer <andre.philippe.sailer@cern.ch>
2023-10-11 09:26:30 +0200

    Test: set detector model, fix inputFileS

--- test/test_CLDConfig.py
+++ test/test_CLDConfig.py
@@ -11,5 +11,8 @@ def _CLDConfig(test_fun):
 
 @_CLDConfig
 def test_lcio_edm4hep():
-    res = subprocess.run("k4run --inputFile=../test.slcio --outputBasename=rec_test CLDReconstruction.py".split(), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    command = "k4run --inputFiles=../test.slcio --outputBasename=rec_test CLDReconstruction.py".split()
+    detectorModel = os.path.join(os.environ["K4GEO"], "FCCee/CLD/compact/FCCee_o1_v04/FCCee_o1_v04.xml")
+    command.extend(["--GeoSvc.detectors", detectorModel])
+    res = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
     assert res.returncode == 0, res.stdout
